bd72eb8a4d2e681cdfa68778bd1a63020dd6f93a
kishore <kishorebittu3003@gmail.com>
2022-10-14 18:34:02 +0530

    123

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "again" {
+  bucket = "my-mani-bucket"
+  }
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIATIXFJWPI75DZXX2D"
+  secret_key = "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"
+}
